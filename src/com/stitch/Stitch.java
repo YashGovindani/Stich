23a0530455afa0a9c2c25d023c8eb612786327c9
YashGovindani <yashgovindani222@gmail.com>
2021-07-20 20:09:04 +0530

    root servlet

---
+++ src/com/stitch/Stitch.java
@@ -0,0 +1,54 @@
+package com.stitch;
+
+import com.stitch.exception.*;
+import com.stitch.annotation.*;
+import com.stitch.pojo.*;
+import com.stitch.model.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class Stitch extends HttpServlet
+{
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    {
+        doGet(request, response);
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    {
+        try
+        {
+            String requestURI = request.getRequestURI();
+            String servletPath = request.getServletPath();
+            String path = requestURI.substring(requestURI.indexOf(servletPath) + servletPath.length());
+            ServletContext servletContext = getServletContext();
+            StitchModel model = (StitchModel)servletContext.getAttribute("model");
+            if(model.has(path))
+            {
+                Service service = model.get(path);
+                String requestMethod = request.getMethod();
+                if(requestMethod.equals("GET") && service.getRequestMethod()!=service.GET)
+                {
+                    response.sendError(response.SC_METHOD_NOT_ALLOWED);
+                    return;
+                }
+                if(requestMethod.equals("POST") && service.getRequestMethod()!=service.POST)
+                {
+                    response.sendError(response.SC_METHOD_NOT_ALLOWED);
+                    return;
+                }
+                Object object = service.getServiceClass().getConstructor().newInstance();
+                service.getService().invoke(object, new Object[0]);
+                if(service.getForwardTo().length() == 0) return;
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher(service.getForwardTo());
+                requestDispatcher.forward(request, response);
+                return;
+            }
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
+            requestDispatcher.forward(request, response);
+        }catch(Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+}
