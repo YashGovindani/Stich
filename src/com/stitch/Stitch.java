fa9d1f29e64b3101a62f5e8c6923a7c03bccfcbc
YashGovindani <yashgovindani222@gmail.com>
2021-07-22 16:08:07 +0530

    code to inject

--- src/com/stitch/Stitch.java
+++ src/com/stitch/Stitch.java
@@ -7,6 +7,7 @@ import com.stitch.model.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.lang.reflect.*;
 
 public class Stitch extends HttpServlet
 {
@@ -22,6 +23,7 @@ public class Stitch extends HttpServlet
             String servletPath = request.getServletPath();
             String path = requestURI.substring(requestURI.indexOf(servletPath) + servletPath.length());
             ServletContext servletContext = getServletContext();
+            HttpSession session = request.getSession();
             StitchModel model = (StitchModel)servletContext.getAttribute("model");
             if(model.has(path))
             {
@@ -37,7 +39,40 @@ public class Stitch extends HttpServlet
                     response.sendError(response.SC_METHOD_NOT_ALLOWED);
                     return;
                 }
-                Object object = service.getServiceClass().getConstructor().newInstance();
+                Class serviceClass = service.getServiceClass();
+                Object object = serviceClass.getConstructor().newInstance();
+                if(service.injectApplicationDirectory())
+                {
+                    Field field = serviceClass.getDeclaredField("applicationDirectory");
+                    if(field == null) throw new ServiceException("For service : " + path + ", Service class : " + serviceClass + " should have property : applicationDirectory of type : " + ApplicationDirectory.class + " to inject application directory");
+                    if(!field.getType().equals(ApplicationDirectory.class)) throw new ServiceException("For service : " + path + ", Service class : " + serviceClass + " should have property : applicationDirectory of type : " + ApplicationDirectory.class + " to inject application directory");
+                    Method method = serviceClass.getMethod("setApplicationDirectory",new Class[]{ApplicationDirectory.class});
+                    method.invoke(object, new Object[]{new ApplicationDirectory(new File(servletContext.getRealPath("")))});
+                }
+                if(service.injectApplicationScope())
+                {
+                    Field field = serviceClass.getDeclaredField("applicationScope");
+                    if(field == null) throw new ServiceException("For service : " + path + ", Service class : " + serviceClass + " should have property : applicationScope of type : " + ApplicationScope.class + " to inject application scope");
+                    if(!field.getType().equals(ApplicationScope.class)) throw new ServiceException("For service : " + path + ", Service class : " + serviceClass + " should have property : applicationScope of type : " + ApplicationScope.class + " to inject application scope");
+                    Method method = serviceClass.getMethod("setApplicationScope",new Class[]{ApplicationScope.class});
+                    method.invoke(object, new Object[]{new ApplicationScope(servletContext)});
+                }
+                if(service.injectRequestScope())
+                {
+                    Field field = serviceClass.getDeclaredField("requestScope");
+                    if(field == null) throw new ServiceException("For service : " + path + ", Service class : " + serviceClass + " should have property : requestScope of type : " + RequestScope.class + " to inject request scope");
+                    if(!field.getType().equals(RequestScope.class)) throw new ServiceException("For service : " + path + ", Service class : " + serviceClass + " should have property : requestScope of type : " + RequestScope.class + " to inject request scope");
+                    Method method = serviceClass.getMethod("setRequestScope",new Class[]{RequestScope.class});
+                    method.invoke(object, new Object[]{new RequestScope(request)});
+                }
+                if(service.injectSessionScope())
+                {
+                    Field field = serviceClass.getDeclaredField("sessionScope");
+                    if(field == null) throw new ServiceException("For service : " + path + ", Service class : " + serviceClass + " should have property : sessionScope of type : " + SessionScope.class + " to inject session scope");
+                    if(!field.getType().equals(SessionScope.class)) throw new ServiceException("For service : " + path + ", Service class : " + serviceClass + " should have property : sessionScope of type : " + SessionScope.class + " to inject session scope");
+                    Method method = serviceClass.getMethod("setSessionScope",new Class[]{SessionScope.class});
+                    method.invoke(object, new Object[]{new SessionScope(session)});
+                }
                 service.getService().invoke(object, new Object[0]);
                 if(service.getForwardTo().length() == 0) return;
                 RequestDispatcher requestDispatcher = request.getRequestDispatcher(service.getForwardTo());
