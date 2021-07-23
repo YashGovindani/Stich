0477aa0522ceb74a68699e8c927e3ef40e8f5f72
YashGovindani <yashgovindani222@gmail.com>
2021-07-24 05:04:03 +0530

    annotation added

---
+++ src/com/stitch/annotation/RequestParameter.java
@@ -0,0 +1,10 @@
+package com.stitch.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER})
+public @interface RequestParameter
+{
+    String value();
+}
