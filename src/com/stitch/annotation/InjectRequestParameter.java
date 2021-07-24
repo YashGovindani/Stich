95735e33072ec57dde46d6e07173d234c3806f59
YashGovindani <yashgovindani222@gmail.com>
2021-07-24 17:49:43 +0530

    annotation to inject request parameter

---
+++ src/com/stitch/annotation/InjectRequestParameter.java
@@ -0,0 +1,10 @@
+package com.stitch.annotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface InjectRequestParameter
+{
+    String value();
+}
